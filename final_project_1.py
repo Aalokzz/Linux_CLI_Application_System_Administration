a6ab80181ca16cccbbb978af702d5a744fdff685
Aalok Dhonju <133177228+Aalokzz@users.noreply.github.com>
2024-11-27 18:42:54 -0500

    Add files via upload

---
+++ final_project_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# Importing Modules For The Program
+
+import argparse
+import subprocess
+import os
+import time
+
+# Function to check CPU and Memory Usage
+def system_usage():
+    pass
+
+# Function to check Disk Space Usage
+def disk_space_usage():
+    pass
+
+def main():
+
+
+
+
+
+
